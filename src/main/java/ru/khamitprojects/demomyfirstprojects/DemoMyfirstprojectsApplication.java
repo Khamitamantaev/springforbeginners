ca03369d57854abd59b5a6f2192f8592bf910589
Khamit <khamitamantaev@gmail.com>
2019-11-30 23:25:43 +0500

    add full Spring Generate Project to my repository

---
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/DemoMyfirstprojectsApplication.java
@@ -0,0 +1,13 @@
+package ru.khamitprojects.demomyfirstprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMyfirstprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMyfirstprojectsApplication.class, args);
+	}
+
+}
