03236d990fbbd7cd52ce75eff588dcc265d014d7
Khamit <khamitamantaev@gmail.com>
2019-12-03 01:02:51 +0500

    самое интересное! я создал поля, которые будут указывать поля таблицы, поле id генерируется автоматически , поле user должно быть уникальным и не равнаяться нулю, и пароль просто не равняться нулю

--- src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/User.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/User.java
@@ -1,9 +1,18 @@
 package ru.khamitprojects.demomyfirstprojects.persist.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+   @Column(unique = true , nullable = false)
+    private String username;
+
+   @Column(nullable = false)
+    private String password;
 }
