e39d766786e38a8fb260a4777c579774d3958407
Khamit <khamitamantaev@gmail.com>
2019-12-02 18:40:11 +0500

    пустой конструктор и геттеры с сеттерами задаем

--- src/main/java/ru/khamitprojects/demomyfirstprojects/repr/UserRepr.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/repr/UserRepr.java
@@ -13,4 +13,39 @@ public class UserRepr {
 
     @NotEmpty
     private String matchingPassword;
+
+    public UserRepr() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
 }
