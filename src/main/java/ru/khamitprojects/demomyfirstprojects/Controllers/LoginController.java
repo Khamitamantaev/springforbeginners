b691adf79ce6494347f35fd2a2782b6abec1ea91
Khamit <khamitamantaev@gmail.com>
2019-12-02 20:45:40 +0500

    с помощью этого класса модель, контроллер может взаимодействовать с представлением и передавать в него какую нибудь информацию

--- src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/LoginController.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/LoginController.java
@@ -1,7 +1,9 @@
 package ru.khamitprojects.demomyfirstprojects.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -12,10 +14,14 @@ public class LoginController {
     }
 
     @GetMapping("/register")
-    public String registerPage() {
+    public String registerPage(Model model) {
         return "register";
     }
 
+    @PostMapping("/register")
+    public String registerNewUser() {
+        return "register";
+    }
 
 
 }
