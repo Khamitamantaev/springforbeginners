a66bb505be16189c02f3b90665705f10ad24936b
Khamit <khamitamantaev@gmail.com>
2019-12-02 21:16:39 +0500

    Создаем некого пустого пользователя , чтобы передать представлению

--- src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/LoginController.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/LoginController.java
@@ -3,7 +3,11 @@ package ru.khamitprojects.demomyfirstprojects.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.khamitprojects.demomyfirstprojects.repr.UserRepr;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -15,6 +19,7 @@ public class LoginController {
 
     @GetMapping("/register")
     public String registerPage(Model model) {
+        model.addAttribute("user", new UserRepr());
         return "register";
     }
 
