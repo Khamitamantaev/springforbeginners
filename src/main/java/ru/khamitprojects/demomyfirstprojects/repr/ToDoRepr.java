49810d80fa03581815e8cfc3ad859fb0304e49b8
Khamit <khamitamantaev@gmail.com>
2019-12-02 18:29:22 +0500

    Это представления todo записей
    как я понимаю, это шаблон с помощью которого будут создаваться todo обьекты и которые будут после добавляться в список записей на главной странице после нажатия кнопки add todo к примеру

--- src/main/java/ru/khamitprojects/demomyfirstprojects/repr/ToDoRepr.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/repr/ToDoRepr.java
@@ -1,5 +1,48 @@
 package ru.khamitprojects.demomyfirstprojects.repr;
 
 
-public class ToDoRepr {
+import java.time.LocalDate;
+
+public class ToDoRepr { // будет содержать представления todo записи
+    private long id;
+    private String description;
+
+    private String username;
+
+    private LocalDate targetDate;
+
+    public ToDoRepr() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(LocalDate targetDate) {
+        this.targetDate = targetDate;
+    }
 }
