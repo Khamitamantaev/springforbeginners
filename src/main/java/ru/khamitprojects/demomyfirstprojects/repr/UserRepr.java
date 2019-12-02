3ad0f187723210593068bbc0fe4d16dc26f03e02
Khamit <khamitamantaev@gmail.com>
2019-12-02 18:39:15 +0500

    Это класс представления , который будет отправляться при регистрации пользователя

--- src/main/java/ru/khamitprojects/demomyfirstprojects/repr/UserRepr.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/repr/UserRepr.java
@@ -1,4 +1,16 @@
 package ru.khamitprojects.demomyfirstprojects.repr;
 
+import javax.validation.constraints.NotEmpty;
+
 public class UserRepr {
+    private long id;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String matchingPassword;
 }
