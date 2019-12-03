124ba07cc8d1f46bfafe5b2cbbd43e2e0e4e6302
Khamit <khamitamantaev@gmail.com>
2019-12-03 17:40:22 +0500

    DaoAuthenticationProvider будет нам указывать, каким образом должен происходить механизм авторизации пользователей

--- src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package ru.khamitprojects.demomyfirstprojects.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,4 +15,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
     }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userAuthService);
+        auth.setPasswordEncoder(passwordEncoder);
+        return auth;
+    }
+
 }
