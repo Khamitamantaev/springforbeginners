0d111e00eff8dd0f40006251a9fdd4fd172b3102
Khamit <khamitamantaev@gmail.com>
2019-12-03 17:08:12 +0500

    теперь, с помощью метода encode, полученный пароль будет кодироваться и отправляться в базу данных в закодированном виде

--- src/main/java/ru/khamitprojects/demomyfirstprojects/service/UserService.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.khamitprojects.demomyfirstprojects.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.khamitprojects.demomyfirstprojects.persist.entity.User;
 import ru.khamitprojects.demomyfirstprojects.persist.repo.UserRepository;
@@ -13,15 +14,18 @@ import javax.transaction.Transactional;
 public class UserService {
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void create(UserRepr userRepr){
         User user = new User();
         user.setUsername(userRepr.getUsername());
-        user.setPassword(userRepr.getPassword());
+        user.setPassword(passwordEncoder.encode(userRepr.getPassword()));
         userRepository.save(user);
 
     }
