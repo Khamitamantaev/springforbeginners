af85b99a62c5a0756cbbf873b370a087d9371e58
Khamit <khamitamantaev@gmail.com>
2019-12-03 16:51:50 +0500

    EnableWebSecurity аннотация говорит SpringBoot-у что мы хотим SpringSecurity использовать

--- src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package ru.khamitprojects.demomyfirstprojects.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
