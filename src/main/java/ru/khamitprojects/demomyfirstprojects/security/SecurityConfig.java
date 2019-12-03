07321a0dee2ab3c297d7d9f1074300dc29002e51
Khamit <khamitamantaev@gmail.com>
2019-12-03 17:43:50 +0500

    благодаря этому сеттеру , сюда будет внедрен passwordEncoder из класса Main

--- src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/security/SecurityConfig.java
@@ -1,15 +1,23 @@
 package ru.khamitprojects.demomyfirstprojects.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,5 +31,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.setPasswordEncoder(passwordEncoder);
         return auth;
     }
-
 }
