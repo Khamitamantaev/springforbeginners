024d5e1485333fbeb26710a5ed21192ef74b27e9
Khamit <khamitamantaev@gmail.com>
2019-12-03 17:00:13 +0500

    бин passwordEncoder содержит в себе функционал, который позволяет кодировать пароль ,сохранять, и осуществлять проверку на правильность

--- src/main/java/ru/khamitprojects/demomyfirstprojects/DemoMyfirstprojectsApplication.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/DemoMyfirstprojectsApplication.java
@@ -2,9 +2,16 @@ package ru.khamitprojects.demomyfirstprojects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DemoMyfirstprojectsApplication {
+	public class DemoMyfirstprojectsApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMyfirstprojectsApplication.class, args);
