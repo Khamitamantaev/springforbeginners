f97e1c4147939fff6bd7657535e6d62ac6199d9a
Khamit <khamitamantaev@gmail.com>
2019-12-04 18:07:40 +0500

    Ааннотация @table в которой мы указываем имя таблицы , в которой будет хранится данная сущность

--- src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
@@ -1,7 +1,9 @@
 package ru.khamitprojects.demomyfirstprojects.persist.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "todos")
 public class ToDo {
 }
