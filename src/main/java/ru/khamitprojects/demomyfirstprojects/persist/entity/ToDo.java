5823bec51e8a2617ec95a72c28a62ed0ecbd8656
Khamit <khamitamantaev@gmail.com>
2019-12-04 18:11:22 +0500

    первое поле сущности идентификатор id такой же, как и в User

--- src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
@@ -1,9 +1,12 @@
 package ru.khamitprojects.demomyfirstprojects.persist.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "todos")
 public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
