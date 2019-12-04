019e625cc8cf69f2be83bf316055f9a08a7ad66a
Khamit <khamitamantaev@gmail.com>
2019-12-05 00:49:22 +0500

    Нужно выводить информацию о конкретном ToDo по его идентификатору

--- src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/TodoController.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/Controllers/TodoController.java
@@ -1,5 +1,6 @@
 package ru.khamitprojects.demomyfirstprojects.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.khamitprojects.demomyfirstprojects.service.UserService;
@@ -9,6 +10,7 @@ public class TodoController {
 
     private UserService userService;
 
+    @Autowired
     public TodoController(UserService userService) {
         this.userService = userService;
     }
@@ -18,7 +20,7 @@ public class TodoController {
         return "index";
     }
 
-    @GetMapping("/todo")
+    @GetMapping("/todo/{id}")
     public String todoPage() {
         return "todo";
     }
