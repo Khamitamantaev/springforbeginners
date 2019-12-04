d219018d8f65648277acf0b99450050b91f44453
Khamit <khamitamantaev@gmail.com>
2019-12-04 18:19:00 +0500

    поле description = описание задания , какой то конкретный пользователь  и время

--- src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
+++ src/main/java/ru/khamitprojects/demomyfirstprojects/persist/entity/ToDo.java
@@ -1,6 +1,7 @@
 package ru.khamitprojects.demomyfirstprojects.persist.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "todos")
@@ -9,4 +10,14 @@ public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private LocalDate targetDate;
 }
